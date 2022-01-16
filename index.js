08297ef131df88fb5647b5964d2c48b72ac8cafc
Henry Miller <mail.henrym+git@gmail.com>
2022-01-15 21:27:55 -0500

    commit tree 4d74384c2a3febe8e7b292508fca261613bc19a7

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// function addBatt (lst) {
+//     tot = 0;
+//     for (batt of lst) {
+//         tot+=batt;
+//     }
+//     return tot;
+// }
+
+// function addBattery (tot,lst) {
+//     return tot+=lst;
+// }
+
+const addBattery = (num1,num2) => {
+    return num1+=num2;
+}
+
+const totalBatteries = batteryBatches.reduce(addBattery,0);
+// const totalBatteries = batteryBatches.reduce((tot,lst) => {return tot+=lst;},0);
